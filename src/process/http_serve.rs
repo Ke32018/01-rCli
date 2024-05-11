65cc57cd196946deec80abe27c280daf7b8d9df0
leike <Ke_lei@foxmail.com>
2024-05-11 17:33:46 +0800

    feature: axum demo with state

--- src/process/http_serve.rs
+++ src/process/http_serve.rs
@@ -1,18 +1,28 @@
 use anyhow::Result;
+use axum::extract::State;
 use axum::{routing::get, Router};
 use std::net::SocketAddr;
-use std::path::Path;
+use std::path::PathBuf;
+use std::sync::Arc;
 use tracing::info;
 
-pub async fn process_http_serve(path: &Path, port: u16) -> Result<()> {
-    let router = Router::new().route("/", get(index_handler));
+#[derive(Debug)]
+struct HttpServeState {
+    path: PathBuf,
+}
+
+pub async fn process_http_serve(path: PathBuf, port: u16) -> Result<()> {
     let addr = SocketAddr::from(([0, 0, 0, 0], port));
-    info!("Serving {:?} on port{}", path, addr);
+    info!("Serving {:?} on port{}", &path, addr);
+    let state = HttpServeState { path };
+    let router = Router::new()
+        .route("/", get(index_handler))
+        .with_state(Arc::new(state));
     let listener = tokio::net::TcpListener::bind(addr).await?;
     axum::serve(listener, router).await?;
     Ok(())
 }
 
-async fn index_handler() -> &'static str {
-    "Hello, World!"
+async fn index_handler(State(state): State<Arc<HttpServeState>>) -> String {
+    format!("Serving {:?}", state.path)
 }
