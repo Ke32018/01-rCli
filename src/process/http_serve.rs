618ffe268ea5216c750e5d4830243ecc77e19aea
leike <Ke_lei@foxmail.com>
2024-05-13 15:26:48 +0800

    feature: using tower_http

--- src/process/http_serve.rs
+++ src/process/http_serve.rs
@@ -3,6 +3,7 @@ use axum::extract::{Path, State};
 use axum::http::StatusCode;
 use axum::{routing::get, Router};
 use std::{net::SocketAddr, path::PathBuf, sync::Arc};
+use tower_http::services::{ServeDir, ServeFile};
 use tracing::{info, warn};
 
 #[derive(Debug)]
@@ -10,12 +11,21 @@ struct HttpServeState {
     path: PathBuf,
 }
 
-pub async fn process_http_serve(path: PathBuf, port: u16) -> Result<()> {
+pub async fn process_http_serve(dir: PathBuf, port: u16) -> Result<()> {
     let addr = SocketAddr::from(([0, 0, 0, 0], port));
-    info!("Serving {:?} on port{}", &path, addr);
-    let state: HttpServeState = HttpServeState { path };
+    info!("Serving {:?} on port{}", &dir, addr);
+    let state: HttpServeState = HttpServeState { path: dir.clone() };
+    let service = ServeDir::new(dir)
+        .append_index_html_on_directories(true)
+        .precompressed_gzip()
+        .precompressed_br()
+        .precompressed_deflate()
+        .precompressed_zstd()
+        .not_found_service(ServeFile::new("assets/not_found.html"));
+
     let router = Router::new()
         .route("/*path", get(file_handler))
+        .nest_service("/tower", service)
         .with_state(Arc::new(state));
     let listener = tokio::net::TcpListener::bind(addr).await?;
     axum::serve(listener, router).await?;
@@ -26,7 +36,7 @@ async fn file_handler(
     State(state): State<Arc<HttpServeState>>,
     Path(path): Path<String>,
 ) -> (StatusCode, String) {
-    // format!("Serving {:?}", state.path)
+    format!("Serving {:?}", state.path);
     let p = std::path::Path::new(&state.path).join(path);
     info!("Reading file {:?}", p);
     if !p.exists() {
@@ -45,5 +55,20 @@ async fn file_handler(
                 (StatusCode::INTERNAL_SERVER_ERROR, e.to_string())
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_file_handler() {
+        let state = Arc::new(HttpServeState {
+            path: PathBuf::from("."),
+        });
+        let (status, content) = file_handler(State(state), Path("Cargo.toml".to_string())).await;
+        assert_eq!(status, StatusCode::OK);
+        assert!(content.contains("anyhow = \"1.0.83\""));
     }
 }
