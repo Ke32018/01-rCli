3a11eca0de233ad1b14745b85d504fdc0cd2ef36
leike <Ke_lei@foxmail.com>
2024-05-13 17:55:53 +0800

    feature: 作业-增加文件服务器索引页面

--- src/process/http_serve.rs
+++ src/process/http_serve.rs
@@ -1,6 +1,7 @@
 use anyhow::Result;
 use axum::extract::{Path, State};
-use axum::http::StatusCode;
+use axum::response::IntoResponse;
+use axum::{http::StatusCode, response::Html};
 use axum::{routing::get, Router};
 use std::{net::SocketAddr, path::PathBuf, sync::Arc};
 use tower_http::services::{ServeDir, ServeFile};
@@ -35,27 +36,60 @@ pub async fn process_http_serve(dir: PathBuf, port: u16) -> Result<()> {
 async fn file_handler(
     State(state): State<Arc<HttpServeState>>,
     Path(path): Path<String>,
-) -> (StatusCode, String) {
+) -> impl IntoResponse {
     format!("Serving {:?}", state.path);
     let p = std::path::Path::new(&state.path).join(path);
+
     info!("Reading file {:?}", p);
     if !p.exists() {
-        (
-            StatusCode::NOT_FOUND,
-            format!("File {} not found", p.display()),
-        )
+        (StatusCode::NOT_FOUND, Html("File not found".to_string())).into_response()
+    } else if p.is_dir() {
+        let body = build_directory_list(&p, &state.path);
+        (StatusCode::OK, Html(body)).into_response()
     } else {
         match tokio::fs::read_to_string(p).await {
             Ok(content) => {
                 info!("Read {} bytes", content.len());
-                (StatusCode::OK, content)
+                (StatusCode::OK, content).into_response()
             }
             Err(e) => {
                 warn!("Errorreading file: {:?}", e);
-                (StatusCode::INTERNAL_SERVER_ERROR, e.to_string())
+                (StatusCode::NOT_FOUND, Html("File not found".to_string())).into_response()
             }
         }
     }
+}
+
+fn build_directory_list(path: &std::path::Path, base_path: &PathBuf) -> String {
+    let mut body = String::new();
+    body.push_str("<html><body><ul>");
+
+    for entry in path.read_dir().unwrap() {
+        let entry = entry.unwrap();
+        let file_name = entry.file_name().to_string_lossy().to_string();
+        let entry_path = entry.path();
+
+        if entry_path.is_dir() {
+            body.push_str(&format!("<li><a href=''>{}</a></li>", file_name));
+            // 递归调用以构建子目录的列表
+            let sub_list = build_directory_list(&entry_path, base_path);
+            body.push_str(&sub_list);
+        } else {
+            // 计算相对路径
+            let relative_path = entry_path
+                .strip_prefix(base_path)
+                .unwrap()
+                .to_str()
+                .unwrap()
+                .to_string();
+            body.push_str(&format!(
+                "<li><a href='/{relative_path}'>{file_name}</a></li>"
+            ));
+        }
+    }
+
+    body.push_str("</ul></body></html>");
+    body
 }
 
 #[cfg(test)]
@@ -67,8 +101,8 @@ mod tests {
         let state = Arc::new(HttpServeState {
             path: PathBuf::from("."),
         });
-        let (status, content) = file_handler(State(state), Path("Cargo.toml".to_string())).await;
-        assert_eq!(status, StatusCode::OK);
-        assert!(content.contains("anyhow = \"1.0.83\""));
+        file_handler(State(state), Path("src".to_string())).await;
+        // assert_eq!(, StatusCode::OK);
+        // assert!(content.trim().starts_with("<!DOCTYPE html>"));
     }
 }
